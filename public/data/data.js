250256a99d3a42ff3dc02b0ee057f4051866c511
Minhho29 <2001140026@s.hanu.edu.vn>
2022-12-05 16:36:31 +0700

    push code

---
+++ public/data/data.js
@@ -0,0 +1,100 @@
+const announcement = [
+  {
+    id: 1,
+    quiz: "The mid-term test",
+    course: "Data structures and Algorithms",
+    TimeStart: "08:00:00 1/12/2022",
+    TimeDo: "45 minutes",
+    Status: "Unfinished",
+  },
+  {
+    id: 2,
+    quiz: "The mid-term test",
+    course: "C++ Object Oriented Programming",
+    TimeStart: "10:00:00 1/12/2022",
+    TimeDo: "45 minutes",
+    Status: "Unfinished",
+  },
+  {
+    id: 3,
+    quiz: "The mid-term test",
+    course: "Database management system",
+    TimeStart: "08:00:00 25/11/2022",
+    TimeDo: "45 minutes",
+    Status: "Done",
+  },
+  {
+    id: 4,
+    quiz: "The mid-term test",
+    course: "Java Basic",
+    TimeStart: "08:00:00 15/01/2022",
+    TimeDo: "45 minutes",
+    Status: "Done",
+  },
+];
+
+const courses = [
+  {
+    id: 1,
+    name: "Data structures and Algorithms",
+    img_url: "./assets/image/ctdlvgt.jpg",
+    discription:
+      "This course will help you understand algorithms as well as understand the nature of data structures - a condition to become a good programmer.",
+    status: "Learning",
+  },
+  {
+    id: 2,
+    name: "Java Basic",
+    img_url: "./assets/image/javabasic.jpg",
+    discription:
+      "Basic Java programming course for beginners, this course will be the foundation for an advanced Java course to progress to Java Web or Android programming, ...",
+    status: "Done",
+  },
+  {
+    id: 3,
+    name: "Python Basic",
+    img_url: "./assets/image/ctdlvgt.jpg",
+    discription:
+      "Basic Python programming course with easy-to-understand exercises and theory, you can confidently move on to more advanced Python topics.",
+    status: "Not Started",
+  },
+  {
+    id: 4,
+    name: "Basic Algorithm",
+    img_url: "./assets/image/algorithmsbasic.jpg",
+    discription:
+      "With the basic algorithm course, you will learn how to think and solve basic programming problems that a programmer needs.",
+    status: "Done",
+  },
+  {
+    id: 5,
+    name: "Database management system",
+    img_url: "./assets/image/sql.jpg",
+    discription:
+      "This course will help you understand algorithms as well as understand the nature of data structures - a condition to become a good programmer.",
+    status: "Learning",
+  },
+  {
+    id: 6,
+    name: "Cloud computing",
+    img_url: "./assets/image/ctdlvgt.jpg",
+    discription:
+      "The course will provide a basic overview and overview of the field of cloud computing and prominent cloud computing technologies.",
+    status: "Not Started",
+  },
+  {
+    id: 7,
+    name: "Computer hardware",
+    img_url: "./assets/image/ctdlvgt.jpg",
+    discription: "This course focuses on providing general knowledge of computer components that you can apply to the selection and use of computers.",
+    status: "Learning",
+  },
+  {
+    id: 8,
+    name: "C++ Advanced",
+    img_url: "./assets/image/ctdlvgt.jpg",
+    discription:
+      "This course will help you understand algorithms as well as understand the nature of data structures - a condition to become a good programmer.",
+    status: "Not Started",
+  },
+];
